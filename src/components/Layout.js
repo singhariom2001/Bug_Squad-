ce2f432f3f2e446ed88d23b6432a43eca719be3d
ahuja palak <ahuja.palak@TL206.triconhome.local>
2024-09-25 11:47:43 +0530

    layout chnged

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom'; 
 import Header from './Header';
 
 const Layout = ({ children }) => {
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Sidebar */}
-      <aside className="w-64 bg-white shadow-md">
+      <aside className="w-44 bg-blue-500 shadow-md">
         <div className="p-6">
-          <h1 className="text-2xl font-bold">Taylor & Francis Group</h1>
+          <h1 className="text-xl font-bold">Taylor & Francis Group</h1>
+          <hr></hr>
         </div>
         <nav className="p-4">
           <ul>
             <li className="mb-2">
-              <Link to="/" className="text-blue-600 hover:text-blue-800">Licenses</Link> {/* Update href to Link */}
-            </li>
-            <li>
-              <Link to="/account-details" className="text-gray-700 hover:text-gray-900">Account Details</Link> {/* Placeholder for another route */}
+              <Link to="/" className="text-black font-bold text-base hover:text-black-800 ml-[40px]">Licenses</Link> 
             </li>
           </ul>
         </nav>
       </aside>
-
-      {/* Main Content */}
       <main className="flex-1 p-6 overflow-y-auto">
         <Header />
         {children}
